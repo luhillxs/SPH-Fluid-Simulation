5677b25b4b89cc38635a1c3b359dbb8197f387c5
Hill Lo <hilllocanton@gmail.com>
2016-04-16 21:19:52 -0400

    upload initial files

---
+++ OpenGL/OpenGL/OpenGL/main.cpp
@@ -0,0 +1,46 @@
+#include <Windows.h>
+#include <GL\glew.h>
+#include <GL\freeglut.h>
+#include <iostream>
+
+using namespace std;
+
+void changeViewPort(int w, int h)
+{
+	glViewport(0, 0, w, h);
+}
+
+void render()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+
+
+int main(int argc, char* argv[]) {
+
+	// Initialize GLUT
+	glutInit(&argc, argv);
+	// Set up some memory buffers for our display
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	// Set the window size
+	glutInitWindowSize(800, 600);
+	// Create the window with the title "Hello,GL"
+	glutCreateWindow("HelloWorld");
+	// Bind the two functions (above) to respond when necessary
+	glutReshapeFunc(changeViewPort);
+	glutDisplayFunc(render);
+
+	// Very important!  This initializes the entry points in the OpenGL driver so we can 
+	// call all the functions in the API.
+	GLenum err = glewInit();
+	if (GLEW_OK != err) {
+		fprintf(stderr, "GLEW error");
+		return 1;
+	}
+
+
+	glutMainLoop();
+	return 0;
+}
